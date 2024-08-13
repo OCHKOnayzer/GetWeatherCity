f1654bee319b3bf835bc2a78f97840a3c642c7ee
Антон <125889571+OCHKOnayzer@users.noreply.github.com>
2024-08-13 13:20:09 +0300

    Update http API file

--- src/components/http/index.js
+++ src/components/http/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_WEATHER_KEY = 'e7b9354c6a7c69311fef772ab2cb313e';
+const API_WEATHER_KEY = '';//your api key
 
 const API_WEATHER = axios.create({
     baseURL: `http://api.openweathermap.org/data/2.5/`,
